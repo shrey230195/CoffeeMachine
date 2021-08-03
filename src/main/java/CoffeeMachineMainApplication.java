c1c66cdeb224dc350597d09059795be3db50cbdd
shrey230195 <shreydixit93@gmail.com>
2021-08-03 13:00:43 +0530

    start: initialising java project

---
+++ src/main/java/CoffeeMachineMainApplication.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class CoffeeMachineMainApplication {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Arguments are required");
+        }
+
+        System.out.println("Application started");
+    }
+}
